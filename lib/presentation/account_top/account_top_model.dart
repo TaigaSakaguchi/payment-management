177afae8f0f6f5a2ec88d2b0fc3dd1dba51604b9
TaigaSakaguchi <gucci.tennismath@gmail.com>
2021-06-12 22:33:40 +0900

    不要文削除

--- lib/presentation/account_top/account_top_model.dart
+++ lib/presentation/account_top/account_top_model.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
 import 'package:tatekae/domain/payment.dart';
